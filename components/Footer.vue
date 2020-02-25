cac7054fcb3ba20a2888bb1879f2fe10fa915053
Wright <wrightkhlebisol@gmail.com>
2020-02-25 14:58:04 +0100

    Worked on the footer section

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,32 @@
 <template>
-  <b-row align-v="end">
-    <b-col>One of three columns</b-col>
+  <b-row align-v="end" class="footer">
+    <b-col>
+      <b-row>
+        <b-col>
+          Download App
+          <img src="/desktop logo optimized.svg" />
+          <img src="/desktop logo optimized.svg" />
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-col>
+    <b-col>
+      <b-row>
+        <b-col>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perspiciatis nemo animi et ad atque dolorum molestias voluptatem commodi, harum deleniti voluptates consectetur consequatur.</b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <a href>Lorem, ipsum dolor sit</a>
+        </b-col>
+        <b-col>
+          <a href>Lorem, ipsum dolor sit</a>
+        </b-col>
+        <b-col>
+          <a href>Lorem, ipsum dolor sit</a>
+        </b-col>
+      </b-row>
+    </b-col>
+    <b-col>3 of 3</b-col>
   </b-row>
 </template>
 
@@ -9,4 +35,10 @@ export default {}
 </script>
 
 <style scoped>
+.footer {
+  background-color: #f4f7f6;
+}
+/* b-card {
+  width: 100%;
+} */
 </style>
